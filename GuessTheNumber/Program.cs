19060b1fa7705179c0e0261a2afed192a6b3c8fb
DiogoJP202 <diogojp202@gmail.com>
2024-12-19 02:35:16 -0300

    Add project files.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int secretNumber = random.Next(1, 101);
+int userGuess = 0;
+int count = 0;
+
+while(userGuess != secretNumber)
+{
+    Console.WriteLine("Enter your guess: ");
+    userGuess = int.Parse(Console.ReadLine());
+    count++;
+
+    if(userGuess < secretNumber)
+    {
+        Console.WriteLine("Too low! Try again.");
+    } else if(userGuess > secretNumber)
+    {
+        Console.WriteLine("Too high! Try again.");
+    } else
+    {
+        Console.WriteLine($"Congratulations! You guessed the number right! It took you {count} tries.");
+    }
+}
+
+Console.ReadKey();
